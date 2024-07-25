7e3cadb8463d1b5957244819f9a0b8c6c4e6eb18
mculha <melih.culha.09@gmail.com>
2024-07-25 23:09:05 +0300

    Initial Commit

---
+++ SwiftUIInAppTranslation/SwiftUIInAppTranslationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInAppTranslationApp.swift
+//  SwiftUIInAppTranslation
+//
+//  Created by Melih Çulha on 25.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInAppTranslationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
